28e23234006d01bbc67b6f09835b5b2c3a47591d
Priyanshu <gargpriyanshu2004@gmail.com>
2025-07-17 01:32:03 +0530

    db migration

--- app/models/users.py
+++ app/models/users.py
@@ -12,7 +12,7 @@ class Users(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     username = Column(String(80), nullable=False)
-    org_id = Column(Integer, ForeignKey('organizations.id'), nullable=False)
+    org_id = Column(Integer, nullable=False)
     role = Column(String(32), nullable=False)
     password_hash = Column(String(128), nullable=False)
     email = Column(String(120), nullable=True, default=None)
