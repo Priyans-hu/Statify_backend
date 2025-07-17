c15e7239e63069737ac9048c5bffe00b7cbe2c53
Priyanshu <gargpriyanshu2004@gmail.com>
2025-07-17 23:58:43 +0530

    added new tables

---
+++ app/models/organizations.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy.sql import func
+from app.database import Base
+
+class Organizations(Base):
+    __tablename__ = 'organizations'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+    is_deleted = Column(Boolean, default=False)
---
+++ app/models/status_master.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+class StatusMaster(Base):
+    __tablename__ = 'status_master'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    status = Column(String(100), nullable=False)
